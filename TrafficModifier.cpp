de46a20a5d3761077f646680e75c86181d8dbd58
Michał Kocon <nobooya@gmail.com>
2017-04-21 03:12:51 +0200

    Version 2: Working hidden channel

--- TrafficModifier.cpp
+++ TrafficModifier.cpp
@@ -1,28 +1,3 @@
-/*
-* passthru.c
-* (C) 2013, all rights reserved,
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Lesser General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-/*
-* DESCRIPTION:
-* This program does nothing except divert packets and re-inject them.  This is
-* useful for performance testing.
-*
-* usage: netdump.exe windivert-filter num-threads
-*/
 
 #include <winsock2.h>
 #include <windows.h>
@@ -37,42 +12,23 @@
 
 
 void setHiddenMessage(PWINDIVERT_IPHDR ip_header, PWINDIVERT_TCPHDR tcp_header);
-bool getTcpHandshake(PWINDIVERT_TCPHDR tcp_header, bool seed_initialized);
-
-
-std::ifstream message;
-UINT16 key;
-
-
 static DWORD passthru(LPVOID arg);
 
-/*
-* Entry.
-*/
 int __cdecl main(int argc, char **argv)
 {
 	int num_threads = 1;
 	HANDLE handle, thread;
 
-
-	// Divert traffic matching the filter:
-	//handle = WinDivertOpen(argv[1], WINDIVERT_LAYER_NETWORK, 0, 0);
 	handle = WinDivertOpen(
-		"outbound && "              // Outbound traffic only
-		"ip && "                    // Only IPv4 supported
-		"tcp.SrcPort == 8000",     // HTTP (port 80) only
-								   //"tcp.PayloadLength > 0",    // TCP data packets only
+		"outbound && "              // Outbound traffic
+		"ip && "                    // Only IPv4
+		"tcp.SrcPort == 8000 &&"		// port 8000
+		"tcp.PayloadLength > 0",    // TCP data packets only
 		WINDIVERT_LAYER_NETWORK, 0, 0
 	);
 	if (handle == INVALID_HANDLE_VALUE)
 	{
-		if (GetLastError() == ERROR_INVALID_PARAMETER)
-		{
-			fprintf(stderr, "error: filter syntax error\n");
-			exit(EXIT_FAILURE);
-		}
-		fprintf(stderr, "error: failed to open the WinDivert device (%d)\n",
-			GetLastError());
+		std::cerr << "error: failed to open the WinDivert device: " << GetLastError() << std::endl;
 		exit(EXIT_FAILURE);
 	}
 
@@ -95,7 +51,6 @@ int __cdecl main(int argc, char **argv)
 	return 0;
 }
 
-// Passthru thread.
 static DWORD passthru(LPVOID arg)
 {
 	unsigned char packet[MAXBUF];
@@ -104,46 +59,62 @@ static DWORD passthru(LPVOID arg)
 	HANDLE handle = (HANDLE)arg;
 	PWINDIVERT_IPHDR ip_header;
 	PWINDIVERT_TCPHDR tcp_header;
-	message.open("antygona.txt", std::ios::in | std::ios::binary);
+	
+
+	bool addr_initialized = false;
+	UINT32 dst_addr;
 	// Main loop:
 	while (TRUE)
 	{
 		// Read a matching packet.
 		if (!WinDivertRecv(handle, packet, sizeof(packet), &addr, &packet_len))
 		{
-			fprintf(stderr, "warning: failed to read packet (%d)\n",
-				GetLastError());
+			std::cerr << "Message read error: " << GetLastError() << std::endl;
 			continue;
 		}
+		
 		WinDivertHelperParsePacket(packet, packet_len, &ip_header, NULL, NULL, NULL, &tcp_header, NULL, NULL, NULL);
-		setHiddenMessage(ip_header, tcp_header);
+		if (!addr_initialized) dst_addr = ip_header->DstAddr; //get 1st connected addr as intercept server
+		if (ip_header->DstAddr == dst_addr)	setHiddenMessage(ip_header, tcp_header); //if ip addr matches, include hidden message
+
 		// Re-inject the matching packet.
-			if (!WinDivertSend(handle, packet, packet_len, &addr, NULL))
-			{
-				fprintf(stderr, "warning: failed to reinject packet (%d)\n",
-					GetLastError());
-			}
-			//for (int i = 0; i < packet_len; ++i) std::cout << packet[i];
-			//std::cout << std::endl;
+		if (!WinDivertSend(handle, packet, packet_len, &addr, NULL)){
+			std::cerr << "Message send error: " << GetLastError() << std::endl;
+			continue;
+		}
+
 	}
 }
-
+//hides message in tcp/ip packet
 void setHiddenMessage(PWINDIVERT_IPHDR ip_header, PWINDIVERT_TCPHDR tcp_header) {
-	std::cout << std::hex << ip_header->Id;
-	char buffer[2];
-	message.read(buffer, 2);
-	UINT16* ms;
-	ms = (UINT16*)buffer;
-	//*ms = *ms^ key ^ key;
-	ip_header->Id = *ms;
-	std::cout << std::hex << " -> " << ip_header->Id << std::endl;
-}
+	static char byte; // byte from file
+	static int bit_num = 8; // bit to write in byte
+	static std::ifstream message; 
+	static UINT16 id_difference = 0; //difference between actual and changed ip.id. This ensures 2 packets won't have the same id
 
-bool getTcpHandshake(PWINDIVERT_TCPHDR tcp_header, bool seed_initialized) {
-	if (tcp_header->Syn && tcp_header->Ack) {
-		srand(tcp_header->SeqNum);
-		key = rand();
-		return true;
+	if (bit_num == 8) {
+		if(!message.is_open()) message.open("antygona.txt", std::ios::in | std::ios::binary);
+
+		message.read(&byte, 1);
+		bit_num = 0;
+
+		if (message.eof()) { //loop writing hidden message from file
+			message.close();
+			message.open("antygona.txt", std::ios::in | std::ios::binary);
+		}
 	}
-	else return false;
+	
+	if (bit_num < 8) {
+		char bit = 0x01 & (byte >> bit_num);
+		if (bit == 1) {
+			++id_difference; // +2 to ip id when bit is set to 1
+		}
+		else {} 
+		++bit_num;
+		//id field is saved backwards, so we need to add 1st byte of ipheader->id to 2nd byte of id_difference and vice versa
+		char* id = reinterpret_cast<char*>(&(ip_header->Id));
+		char* diff = reinterpret_cast<char*>(&id_difference);
+		id[0] += diff[1];
+		id[1] += diff[0];
+	}
 }
