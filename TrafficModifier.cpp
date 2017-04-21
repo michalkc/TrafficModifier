aae41cb4b9384002bea834119b0900b67e2d78ad
Michał Kocon <nobooya@gmail.com>
2017-04-21 12:02:23 +0200

    Add synchronization with system IP ID counter

--- TrafficModifier.cpp
+++ TrafficModifier.cpp
@@ -9,7 +9,7 @@
 #define MAXBUF  0xFFFF
 
 
-void setHiddenMessage(PWINDIVERT_IPHDR ip_header, PWINDIVERT_TCPHDR tcp_header);
+void setHiddenMessage(PWINDIVERT_IPHDR ip_header, PWINDIVERT_TCPHDR tcp_header, UINT payload_len);
 static DWORD passthru(LPVOID arg);
 
 int __cdecl main(int argc, char **argv)
@@ -20,8 +20,9 @@ int __cdecl main(int argc, char **argv)
 	handle = WinDivertOpen(
 		"outbound && "              // Outbound traffic
 		"ip && "                    // Only IPv4
-		"tcp.SrcPort == 8000 &&"		// port 8000
-		"tcp.PayloadLength > 0",    // TCP data packets only
+	//	"tcp.SrcPort == 8000 &&"		// port 8000
+		"tcp.SrcPort == 8000",		// port 8000
+	//	"tcp.PayloadLength > 0",    // TCP data packets only
 		WINDIVERT_LAYER_NETWORK, 0, 0
 	);
 	if (handle == INVALID_HANDLE_VALUE)
@@ -51,13 +52,12 @@ int __cdecl main(int argc, char **argv)
 static DWORD passthru(LPVOID arg)
 {
 	unsigned char packet[MAXBUF];
-	UINT packet_len;
+	UINT packet_len, payload_len;
 	WINDIVERT_ADDRESS addr;
 	HANDLE handle = (HANDLE)arg;
 	PWINDIVERT_IPHDR ip_header;
 	PWINDIVERT_TCPHDR tcp_header;
 	
-
 	bool addr_initialized = false;
 	UINT32 dst_addr;
 	// Main loop:
@@ -70,9 +70,11 @@ static DWORD passthru(LPVOID arg)
 			continue;
 		}
 		
-		WinDivertHelperParsePacket(packet, packet_len, &ip_header, NULL, NULL, NULL, &tcp_header, NULL, NULL, NULL);
-		if (!addr_initialized) dst_addr = ip_header->DstAddr; //get 1st connected addr as intercept server
-		if (ip_header->DstAddr == dst_addr)	setHiddenMessage(ip_header, tcp_header); //if ip addr matches, include hidden message
+		WinDivertHelperParsePacket(packet, packet_len, &ip_header, NULL, NULL, NULL, &tcp_header, NULL, NULL, &payload_len);
+		if (!addr_initialized) 
+			dst_addr = ip_header->DstAddr; //get 1st connected addr as intercept server
+		if (ip_header->DstAddr == dst_addr)	
+			setHiddenMessage(ip_header, tcp_header, payload_len); //if ip addr matches, include hidden message
 
 		// Re-inject the matching packet.
 		if (!WinDivertSend(handle, packet, packet_len, &addr, NULL)){
@@ -83,11 +85,12 @@ static DWORD passthru(LPVOID arg)
 	}
 }
 //hides message in tcp/ip packet
-void setHiddenMessage(PWINDIVERT_IPHDR ip_header, PWINDIVERT_TCPHDR tcp_header) {
+void setHiddenMessage(PWINDIVERT_IPHDR ip_header, PWINDIVERT_TCPHDR tcp_header, UINT payload_len) {
 	static char byte; // byte from file
 	static int bit_num = 8; // bit to write in byte
 	static std::ifstream message; 
 	static UINT16 id_difference = 0; //difference between actual and changed ip.id. This ensures 2 packets won't have the same id
+	static UINT16 id_last = 0;
 
 	if (bit_num == 8) {
 		if (!message.is_open()) message.open("antygona.txt", std::ios::in | std::ios::binary);
@@ -99,23 +102,35 @@ void setHiddenMessage(PWINDIVERT_IPHDR ip_header, PWINDIVERT_TCPHDR tcp_header) 
 			message.open("antygona.txt", std::ios::in | std::ios::binary);
 		}
 
-		if (!message.is_open) {
+		if (!message.is_open()) {
 			std::cerr << "Couldn't open file" << std::endl;
 			exit(EXIT_FAILURE);
 		}
 	}
-	
-	if (bit_num < 8) {
-		char bit = 0x01 & (byte >> bit_num);
-		if (bit == 1) {
-			++id_difference; // +2 to ip id when bit is set to 1
-		}
-		else {} 
-		++bit_num;
-		//id field is saved backwards, so we need to add 1st byte of ipheader->id to 2nd byte of id_difference and vice versa
-		char* id = reinterpret_cast<char*>(&(ip_header->Id));
-		char* diff = reinterpret_cast<char*>(&id_difference);
-		id[0] += diff[1];
-		id[1] += diff[0];
+	if (payload_len == 0) { //when there is no payload, system often jumps with id num, so we got chance to synchroinize with it 
+		UINT16 diff = ip_header->Id - id_last;
+		if (diff >= id_difference) 
+			id_difference = 0;
+		else 
+			id_difference -= diff;
 	}
+	else{
+		if (bit_num < 8) {
+			char bit = 0x01 & (byte >> bit_num);
+			if (bit == 1) {
+				++id_difference; // +2 to ip id when bit is set to 1
+			}
+			else {}
+			++bit_num;
+		}
+	}
+
+	//id field is saved backwards, so we need to add 1st byte of ipheader->id to 2nd byte of id_difference and vice versa
+	char* id = reinterpret_cast<char*>(&(ip_header->Id));
+	char* diff = reinterpret_cast<char*>(&id_difference);
+	id[0] += diff[1];
+	id[1] += diff[0];
+
+	id_last = ip_header->Id;
+
 }
